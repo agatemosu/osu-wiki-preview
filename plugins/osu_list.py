2874f086e8edf40c3aabea9794ee777cb7067d89
agatemosu <agaatem@outlook.com>
2024-08-03 18:09:23 +0200

    Simplify list plugin

--- plugins/osu_list.py
+++ plugins/osu_list.py
@@ -2,21 +2,20 @@ from markdown_it import MarkdownIt
 from markdown_it.rules_core import StateCore
 
 
-def osu_list(state: StateCore) -> None:
+def osu_ordered_list(state: StateCore) -> None:
     for token in state.tokens:
         if token.type != "ordered_list_open":
             continue
 
-        start = token.attrGet("start")
+        start_attr = token.attrGet("start")
+        start = 0
 
-        if start is None:
-            start = 0
-        else:
-            start -= 1
+        if start_attr is not None:
+            start = start_attr - 1
             token.attrs.pop("start")
 
         token.attrSet("style", f"--list-start: {start}")
 
 
 def osu_list_plugin(md: MarkdownIt) -> None:
-    md.core.ruler.push("osu_list", osu_list)
+    md.core.ruler.push("osu_ordered_list", osu_ordered_list)
