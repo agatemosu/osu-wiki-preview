2774b172c1f53a1f7676af0cc8003721c6cb7510
agatemosu <agaatem@outlook.com>
2024-08-04 19:14:35 +0200

    fix typing in html_modify_content

--- scripts/html_modify_content.py
+++ scripts/html_modify_content.py
@@ -7,9 +7,12 @@ def modify_html_content(html: str, article: str, locale: str) -> str:
     soup = BeautifulSoup(html, "html.parser")
 
     # Fix wikilinks
-    for tag in soup.find_all(["a", "img"]):
+    for tag in soup.select("a, img"):
         if "href" in tag.attrs:
             href = tag["href"]
+
+            if isinstance(href, list):
+                href = "".join(href)
 
             # Skip links that start with "#", "http://", "https://", "mailto:"
             if re.match(r"^(#|https?://|mailto:)", href):
@@ -21,7 +24,13 @@ def modify_html_content(html: str, article: str, locale: str) -> str:
             elif href.startswith("/wiki/"):
                 tag["href"] = f"/wiki/{locale}/{href[6:]}"
 
-        elif "src" in tag.attrs and tag["src"].startswith("img/"):
-            tag["src"] = f"/wiki/{article}/{tag['src']}"
+        elif "src" in tag.attrs:
+            src = tag["src"]
+
+            if isinstance(src, list):
+                src = "".join(src)
+
+            if src.startswith("img/"):
+                tag["src"] = f"/wiki/{article}/{src}"
 
     return str(soup)
