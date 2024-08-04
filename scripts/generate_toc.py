1aadbda6d8436bb94087ce0917e95abc6a4f0b58
agatemosu <agaatem@outlook.com>
2024-08-04 22:24:24 +0200

    Make toc more readeable

--- scripts/generate_toc.py
+++ scripts/generate_toc.py
@@ -1,35 +1,93 @@
-from bs4 import BeautifulSoup
+from typing import Any, Union
 
-from plugins.anchors import slugify
+from bs4 import BeautifulSoup, Tag
+
+
+class TocItem:
+    text: str
+    id: str
+
+    def __init__(self, tag: Tag) -> None:
+        self.text = tag.text
+        self.id = tag.attrs["id"]
+
+
+LevelWithSubItems = tuple[TocItem, list[TocItem]]
+Toc = list[TocItem | LevelWithSubItems]
+
+
+def get_next_el(lst: list, index: int) -> Any | None:
+    if index < len(lst) - 1:
+        return lst[index + 1]
+    else:
+        return None
+
+
+def lvl(tag: Tag) -> int:
+    return int(tag.name[1])
+
+
+def add_item(toc: Toc, el: Tag) -> None:
+    if lvl(el) == 2:
+        toc.append(TocItem(el))
+
+    if lvl(el) == 3:
+        if not isinstance(toc[-1], tuple):
+            raise Exception(f"Type of toc item not correct: {type(toc[-1])}")
+
+        toc[-1][1].append(TocItem(el))
+
+
+def get_toc(html: str) -> Toc:
+    soup = BeautifulSoup(html, "html.parser")
+
+    headings = soup.select("h2, h3")
+    headings_count = len(headings)
+
+    toc: Toc = []
+
+    for i in range(headings_count):
+        curr_el = headings[i]
+        next_el = get_next_el(headings, i)
+
+        if i == 0 and lvl(curr_el) == 3:
+            raise Exception("Toc cannot start with 3")
+
+        if next_el is None:
+            add_item(toc, curr_el)
+            break
+
+        if lvl(curr_el) < lvl(next_el):
+            toc.append((TocItem(curr_el), []))
+            continue
+
+        add_item(toc, curr_el)
+
+    return toc
+
+
+def create_toc_html(
+    items: Union[Toc, LevelWithSubItems],
+    top: bool,
+    small: bool,
+) -> str:
+    html = f'<ul class="wiki-toc-list {"wiki-toc-list--top" if top else ""}">'
+    for item in items:
+        if isinstance(item, TocItem):
+            html += f'<li class="wiki-toc-list__item"><a href="#{item.id}" class="wiki-toc-list__link {"wiki-toc-list__link--small" if small else ""}">{item.text}</a></li>'
+
+        elif isinstance(item, tuple):
+            main_item, sub_items = item
+            html += f'<li class="wiki-toc-list__item"><a href="#{main_item.id}" class="wiki-toc-list__link">{main_item.text}</a>'
+            html += create_toc_html(sub_items, False, True)
+            html += "</li>"
+
+    html += "</ul>"
+    return html
 
 
 def generate_toc(html: str) -> str:
-    soup = BeautifulSoup(html, "html.parser")
+    toc = get_toc(html)
+    toc_html = create_toc_html(toc, True, False)
 
-    toc = '<ol class="wiki-toc-list wiki-toc-list--top">'
-    current_level = 2
-
-    for tag in soup.select("h2, h3"):
-        text = tag.get_text(strip=True)
-        tag_id = tag.get("id") or slugify(text)
-
-        level = int(tag.name[1])
-
-        if level > current_level:
-            toc += '<ol class="wiki-toc-list">'
-        elif level < current_level:
-            toc += "</ol></li>"
-
-        link_class = "wiki-toc-list__link--small" if level == 3 else ""
-
-        toc += f'<li class="wiki-toc-list__item"><a href="#{tag_id}" class="wiki-toc-list__link {link_class}">{text}</a>'
-
-        if level == 3:
-            toc += "</li>"
-
-        current_level = level
-
-    toc += "</ol>"
-    toc += "</li>" + (current_level - 2) * "</ol>"
-
-    return toc
+    return toc_html
