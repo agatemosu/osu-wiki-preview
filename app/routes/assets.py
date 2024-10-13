ad49c2ac4fe8fc9b62c1eb0c74528478bb9fc27b
agatemosu <agaatem@outlook.com>
2024-10-13 14:06:01 +0200

    Simplify flag path

--- app/routes/assets.py
+++ app/routes/assets.py
@@ -7,13 +7,10 @@ bp = Blueprint("assets", __name__, url_prefix="/assets")
 
 @bp.route("/images/flags/<code>.svg")
 async def serve_flag(code: str):
-    flags_dir = "node_modules/@discordapp/twemoji/dist/svg"
-    target_flag_file = f"{code}.svg"
+    flag_dir = "node_modules/@discordapp/twemoji/dist/svg"
+    flag_file = f"{code}.svg"
 
-    if os.path.isfile(f"{flags_dir}/{target_flag_file}"):
-        flag_dir = flags_dir
-        flag_file = target_flag_file
-    else:
+    if not os.path.isfile(f"{flag_dir}/{flag_file}"):
         flag_dir = "resources/flags"
         flag_file = "fallback.svg"
 
